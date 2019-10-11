839aa7eb678a2e7f204f6188a67e3990c9b32785
MEHT9IPA <filippanikin@mail.ru>
2019-10-11 19:33:28 +0300

    L1:CL support

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -19,20 +19,55 @@ namespace Lab_1
                 input = Console.ReadLine();
             }
         }
+        static void Args_incorrect()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Некорректный ввод аргументов консоли, повторите попытку");
+            Console.ResetColor();
+            Console.Write("Нажмите любую клавишу для завершения...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
 
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
             Console.Title = "Аникин Филипп, ИУ5-33Б";
+            double A = 0, B = 0, C = 0, discr;
 
-            double A=0, B=0, C=0, discr;
-            Console.WriteLine("Введите коэффициенты биквадратного уравнения");
-            Console.Write("A = ");
-            Input(ref A);
-            Console.Write("B = ");
-            Input(ref B);
-            Console.Write("C = ");
-            Input(ref C);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Введите коэффициенты биквадратного уравнения");
+                Console.Write("A = ");
+                Input(ref A);
+                Console.Write("B = ");
+                Input(ref B);
+                Console.Write("C = ");
+                Input(ref C);
+                Console.WriteLine("===================");
+            }
+            else if (args.Length == 3)
+            {
+                if (!double.TryParse(args[0], out A))
+                {
+                    Args_incorrect();
+                }
+                if (!double.TryParse(args[1], out B))
+                {
+                    Args_incorrect();
+                }
+                if (!double.TryParse(args[2], out C))
+                {
+                    Args_incorrect();
+                }
+            }
+            else
+            {
+                Args_incorrect();
+            }
 
+            
+            Console.WriteLine($"A={A}; B={B}; C={C}");
+            Console.Write("Нажмите любую клавишу для завершения...");
             Console.ReadKey();
         }
     }
